8845deab21ce43015742e2f4d3770ffcabee3928
PIETTE Benoit <Benoit.PIETTE@ext.ec.europa.eu>
2016-01-13 18:11:48 +0100

    coding exercises

--- java/src/main/java/ctci/chapter2LinkedLists/q4.java
+++ java/src/main/java/ctci/chapter2LinkedLists/q4.java
@@ -1,10 +1,12 @@
 package ctci.chapter2LinkedLists;
 
+import java.util.Random;
+
 /**
  * Created by a502219 on 6/01/2016.
  */
 public class q4 {
-    //TODO
+
     /*
     * Partition
     *
@@ -18,4 +20,50 @@ public class q4 {
     * Output: 3->1->2->10->5->5->8
     *
     * */
+    public static Node partition(int value, Node node) {
+        Node head = node;
+        Node tail = node;
+
+		/* Partition list */
+        while (node != null) {
+            Node next = node.next;
+            if (node.data < value) {
+                /* Insert node at head. */
+                node.next = head;
+                head = node;
+            } else {
+				/* Insert node at tail. */
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        tail.next = null;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(10);
+        Node node = first;
+
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            node.next = new Node(rand.nextInt(20));
+            node = node.next;
+        }
+        displayList(first);
+        Node head = partition(5, first);
+        displayList(head);
+    }
+
+    public static void displayList(Node node) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(node.data);
+        while (node.next != null) {
+            sb.append("->").append(node.data);
+            node = node.next;
+        }
+        System.out.println(sb);
+    }
 }
