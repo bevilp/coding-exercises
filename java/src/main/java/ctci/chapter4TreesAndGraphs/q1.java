edf536d73b7a4a215716f3e673fd7169ef99a42e
PIETTE Benoit <Benoit.PIETTE@ext.ec.europa.eu>
2016-01-20 16:20:05 +0100

    coding exercises

--- java/src/main/java/ctci/chapter4TreesAndGraphs/q1.java
+++ java/src/main/java/ctci/chapter4TreesAndGraphs/q1.java
@@ -1,5 +1,8 @@
 package ctci.chapter4TreesAndGraphs;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Created by a502219 on 6/01/2016.
  */
@@ -12,4 +15,32 @@ public class q1 {
     * Given a directed graph, design an algorithm to find out whether there is a route between nodes.
     *
     * */
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        node1.addAdjacentNode(new Node(2));
+        node1.addAdjacentNode(new Node(3));
+        node1.addAdjacentNode(new Node(4));
+        Node node4 = new Node(5);
+        // node1.addAdjacentNode(node4);
+        Node node2 = new Node(6);
+        node4.addAdjacentNode(node2);
+        System.out.println(BFSSearch(node1, node2));
+    }
+
+    public static boolean BFSSearch(Node root, Node searched) {
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node node = queue.pop();
+            node.visited = true;
+            if (node.equals(searched)) {
+                return true;
+            }
+            // get only the nodes which haven't been visited yet in order to avoid cycles.
+            node.adjacentNodes.parallelStream().filter(node1 -> !node.visited).forEach(node2 -> queue.add(node2));
+            queue.addAll(node.adjacentNodes);
+        }
+        return false;
+    }
 }
