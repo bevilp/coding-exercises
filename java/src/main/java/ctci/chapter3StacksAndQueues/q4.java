35996afaa65721190ef758a282560409b7cded78
PIETTE Benoit <Benoit.PIETTE@ext.ec.europa.eu>
2016-01-20 18:30:50 +0100

    coding exercises

--- java/src/main/java/ctci/chapter3StacksAndQueues/q4.java
+++ java/src/main/java/ctci/chapter3StacksAndQueues/q4.java
@@ -1,5 +1,7 @@
 package ctci.chapter3StacksAndQueues;
 
+import java.util.Stack;
+
 /**
  * q4
  *
@@ -12,4 +14,41 @@ public class q4 {
     *
     * Implement a MyQueue class which implements a queue using two stacks.
     * */
+
+    public static class MyQueue {
+
+        Stack<String> addingStack = new Stack<>();
+
+        Stack<String> removingStack = new Stack<>();
+
+        public boolean isEmpty() {
+            return addingStack.isEmpty() && removingStack.isEmpty();
+        }
+
+        public void add(String data) {
+            addingStack.add(data);
+        }
+
+        public String remove() {
+            if (removingStack.isEmpty()) {
+                while (!addingStack.isEmpty()) {
+                    removingStack.push(addingStack.pop());
+                }
+            }
+            return removingStack.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+
+        for (int i = 0; i < 10; i++) {
+            queue.add("" + i);
+        }
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+
+    }
 }
